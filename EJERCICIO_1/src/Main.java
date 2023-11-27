676fbc4b57ade7dcc70737f2418fadf1cf22a6b6
MarianaMunoz2 <143203307+MarianaMunoz2@users.noreply.github.com>
2023-11-26 21:16:35 -0600

    Add files via upload

---
+++ EJERCICIO_1/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+// Solicitar al usuario dos números enteros positivos, imprimir todos los
+// numeros entre ellos en orden ascendente.
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Introduce el primer numero");
+    }
+}
